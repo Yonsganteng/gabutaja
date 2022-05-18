c2368c3f23a2a292326de0dd714c29619c978a56
Yonsganteng <96860910+Yonsganteng@users.noreply.github.com>
2022-05-19 02:30:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # (©)Codexbotz
 # Recode by @mrismanaziz
+# Clone by @ItsMeYons
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 import logging
@@ -31,8 +32,10 @@ OWNER = os.environ.get("OWNER", "mrismanaziz")
 DB_URI = os.environ.get("DATABASE_URL", "")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "0"))
+FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "0"))
+FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
+FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
